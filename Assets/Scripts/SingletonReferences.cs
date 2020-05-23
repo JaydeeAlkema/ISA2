357425214400ca0d6ae3f901d02bdd906a6559b3
Jaydee Alkema <dev.jaydeealkema@gmail.com>
2020-05-23 18:58:25 +0200

    Add Script

---
+++ Assets/Scripts/SingletonReferences.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// The only purpose of this class is to make it so there is an instance of the master manager when building the game.
+/// </summary>
+public class SingletonReferences : MonoBehaviour
+{
+	[SerializeField] private MasterManager masterManager = default;			// Reference to the master manager.
+}

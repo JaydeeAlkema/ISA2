1eb46d669f614f92d1395ac12527da5313209e14
Jaydee Alkema <dev.jaydeealkema@gmail.com>
2020-05-23 15:52:53 +0200

    Add MasterManager

---
+++ Assets/Scripts/Managers/MasterManager.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "Singletons/MasterManager")]
+public class MasterManager : SingletonScriptableObject<MasterManager>
+{
+	#region Variables
+	[SerializeField] private GameSettings gameSettings = default;
+	#endregion
+
+	#region Properties
+	public static GameSettings GameSettings { get => Instance.gameSettings; }
+	#endregion
+}

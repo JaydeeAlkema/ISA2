40180ba39e064b45429a29438f4fb33d90e1b858
Jaydee Alkema <dev.jaydeealkema@gmail.com>
2020-05-29 14:50:59 +0200

    Add Player Listing Functionality

---
+++ Assets/Scripts/UI/Rooms/PlayerListing.cs
@@ -0,0 +1,24 @@
+﻿using Photon.Realtime;
+using TMPro;
+using UnityEngine;
+
+public class PlayerListing : MonoBehaviour
+{
+	#region Variables
+	[SerializeField] private TextMeshProUGUI text = default;        // Reference to the TextMeshProUGUI Component on the object.
+	[SerializeField] private Player player;
+	#endregion
+
+	#region properties
+	public Player Player { get => player; private set => player = value; }
+	#endregion
+
+	#region Functions
+	public void SetPlayerInfo(Player _player)
+
+	{
+		player = _player;
+		text.text = player.NickName;
+	}
+	#endregion
+}

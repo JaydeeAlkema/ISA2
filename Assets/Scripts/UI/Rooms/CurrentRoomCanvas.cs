1d4347911d7b50f2d318b752481b74bfb349111e
Jaydee Alkema <dev.jaydeealkema@gmail.com>
2020-05-29 14:50:35 +0200

    Add FirstInitialize

--- Assets/Scripts/UI/Rooms/CurrentRoomCanvas.cs
+++ Assets/Scripts/UI/Rooms/CurrentRoomCanvas.cs
@@ -3,6 +3,9 @@
 public class CurrentRoomCanvas : MonoBehaviour
 {
 	#region Variables
+	[SerializeField] private PlayerListingsMenu playerListingsMenu = default;
+	[SerializeField] private LeaveRoomMenu leaveRoomMenu = default;
+
 	private RoomsCanvases roomsCanvases = default;         // Reference to the RoomsCanvases class.s
 	#endregion
 
@@ -10,14 +13,17 @@ public class CurrentRoomCanvas : MonoBehaviour
 	public void FirstInitialize(RoomsCanvases _roomsCanvases)
 	{
 		roomsCanvases = _roomsCanvases;
+		playerListingsMenu.FirstInitialize(_roomsCanvases);
+		leaveRoomMenu.FirstInitialize(_roomsCanvases);
 	}
+
 
 	public void Show()
 	{
 		gameObject.SetActive(true);
 	}
 
-	private void Hide()
+	public void Hide()
 	{
 		gameObject.SetActive(false);
 	}

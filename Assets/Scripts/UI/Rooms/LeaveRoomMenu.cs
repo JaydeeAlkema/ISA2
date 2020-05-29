e728adcd7c5a73ea81d8b3b2afb3464a2f09f5bd
Jaydee Alkema <dev.jaydeealkema@gmail.com>
2020-05-29 14:50:45 +0200

    Add Leave Room Functionality

---
+++ Assets/Scripts/UI/Rooms/LeaveRoomMenu.cs
@@ -0,0 +1,24 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeaveRoomMenu : MonoBehaviour
+{
+	#region Variables
+	private RoomsCanvases roomsCanvases = default;
+	#endregion
+
+	#region Functions
+	public void FirstInitialize(RoomsCanvases canvases)
+	{
+		roomsCanvases = canvases;
+	}
+
+    public void OnClick_LeaveRoom()
+	{
+		PhotonNetwork.LeaveRoom(true);
+		roomsCanvases.CurrentRoomCanvas.Hide();
+	}
+	#endregion
+}

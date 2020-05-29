85eed6bc2f8071c23bf312d859b0b18008d1e1f2
Jaydee Alkema <dev.jaydeealkema@gmail.com>
2020-05-29 14:50:20 +0200

    Add FirstInitialize

--- Assets/Scripts/UI/Rooms/CreateOrJoinRoomCanvas.cs
+++ Assets/Scripts/UI/Rooms/CreateOrJoinRoomCanvas.cs
@@ -4,6 +4,7 @@ public class CreateOrJoinRoomCanvas : MonoBehaviour
 {
 	#region Variables
 	[SerializeField] private CreateRoomMenu createRoomMenu = default;       // Reference to the CreateRoomMenu class.
+	[SerializeField] private RoomListingsMenu roomListingsMenu = default;
 
 	private RoomsCanvases roomsCanvases = default;         // Reference to the RoomsCanvases class.
 	#endregion
@@ -13,6 +14,8 @@ public class CreateOrJoinRoomCanvas : MonoBehaviour
 	{
 		roomsCanvases = _roomsCanvases;
 		createRoomMenu.FirstInitialize(_roomsCanvases);
+
+		roomListingsMenu.FirstInitialize(_roomsCanvases);
 	}
 	#endregion
 }

990fa783bd665e99286000e75fafe29517a855c6
Jaydee Alkema <dev.jaydeealkema@gmail.com>
2020-06-05 14:36:14 +0200

    Add Quick Instantiate

---
+++ Assets/Scripts/Utilities/Networked/QuickInstantiate.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuickInstantiate : MonoBehaviour
+{
+	#region Variables
+	[SerializeField] private GameObject prefab;
+	#endregion
+
+	#region Monobehaviour Callbacks
+	private void Awake()
+	{
+		Vector2 offset = Random.insideUnitCircle * 3f;
+		Vector3 pos = new Vector3(transform.position.x + offset.x, transform.position.y + offset.y, transform.position.z);
+
+		MasterManager.NetworkInstantiate(prefab, pos, Quaternion.identity);
+	}
+	#endregion
+}

d4fd39623392f2fa52fbe772ff7aa368952b075d
Jaydee Alkema <dev.jaydeealkema@gmail.com>
2020-06-05 12:44:50 +0200

    Add Raise Event Example

--- Assets/Scripts/SingleUse/RaiseEventExample.cs
+++ Assets/Scripts/SingleUse/RaiseEventExample.cs
@@ -1,8 +1,12 @@
-﻿using UnityEngine;
+﻿using ExitGames.Client.Photon;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine;
 
-public class RaiseEventExample : MonoBehaviour
+public class RaiseEventExample : MonoBehaviourPun
 {
 	private SpriteRenderer spriteRenderer = default;
+	private const byte COLOR_CHANGE_EVENT = 0;
 
 	private void Awake()
 	{
@@ -11,9 +15,31 @@ public class RaiseEventExample : MonoBehaviour
 
 	private void Update()
 	{
-		if(Input.GetKeyDown(KeyCode.Space))
+		if(base.photonView.IsMine && Input.GetKeyDown(KeyCode.Space))
 		{
 			ChangeColor();
+		}
+	}
+
+	private void OnEnable()
+	{
+		PhotonNetwork.NetworkingClient.EventReceived += NetworkingClient_EventReceived;
+	}
+
+	private void OnDisable()
+	{
+		PhotonNetwork.NetworkingClient.EventReceived -= NetworkingClient_EventReceived;
+	}
+
+	private void NetworkingClient_EventReceived(EventData obj)
+	{
+		if(obj.Code == COLOR_CHANGE_EVENT)
+		{
+			object[] datas = (object[])obj.CustomData;
+			float r = (float)datas[0];
+			float g = (float)datas[1];
+			float b = (float)datas[2];
+			spriteRenderer.color = new Color(r, g, b, 1f);
 		}
 	}
 
@@ -24,5 +50,9 @@ public class RaiseEventExample : MonoBehaviour
 		float b = Random.Range(0f, 1f);
 
 		spriteRenderer.color = new Color(r, g, b, 1f);
+
+		object[] datas = new object[] { r, g, b };
+
+		PhotonNetwork.RaiseEvent(COLOR_CHANGE_EVENT, datas, RaiseEventOptions.Default, SendOptions.SendUnreliable);
 	}
 }

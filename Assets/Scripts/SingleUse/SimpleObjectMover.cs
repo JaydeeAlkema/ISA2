d62ed8a5e3c8c82a5ed6e1485eaf9d370efb6c8a
Jaydee Alkema <dev.jaydeealkema@gmail.com>
2020-06-05 14:37:05 +0200

    Add Simple Object Mover

---
+++ Assets/Scripts/SingleUse/SimpleObjectMover.cs
@@ -0,0 +1,34 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleObjectMover : MonoBehaviourPun
+{
+	[SerializeField] private float movespeed = 1f;
+	private Animator anim;
+
+	private void Awake()
+	{
+		anim = GetComponent<Animator>();
+	}
+
+	private void Update()
+	{
+		if(base.photonView.IsMine)
+		{
+			float x = Input.GetAxisRaw("Horizontal");
+			float y = Input.GetAxisRaw("Vertical");
+			Vector3 finalMoveSpeed = new Vector3(x, y, 0f).normalized * movespeed;
+
+			transform.position += finalMoveSpeed * Time.deltaTime;
+
+			UpdateMovingBoolean((x != 0f || y != 0f));
+		}
+	}
+
+	private void UpdateMovingBoolean(bool moving)
+	{
+		anim.SetBool("Moving", moving);
+	}
+}

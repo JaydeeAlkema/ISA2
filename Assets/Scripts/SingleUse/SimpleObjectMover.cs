404cf01cd6cbc766b8b8061f7eecdca4e957a3a0
Jaydee Alkema <dev.jaydeealkema@gmail.com>
2020-06-25 15:43:02 +0200

    Add Rotation and move functionality

--- Assets/Scripts/SingleUse/SimpleObjectMover.cs
+++ Assets/Scripts/SingleUse/SimpleObjectMover.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 public class SimpleObjectMover : MonoBehaviourPun, IPunObservable
 {
 	[SerializeField] private float movespeed = 1f;
-	private Animator anim;
 
 	public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
 	{
@@ -20,28 +19,35 @@ public class SimpleObjectMover : MonoBehaviourPun, IPunObservable
 		}
 	}
 
-	private void Awake()
-	{
-		anim = GetComponent<Animator>();
-	}
-
 	private void Update()
 	{
 		if(base.photonView.IsMine)
 		{
-			float x = Input.GetAxisRaw("Horizontal");
-			float y = Input.GetAxisRaw("Vertical");
-			Vector3 finalMoveSpeed = new Vector3(x, y, 0f).normalized * movespeed;
-
-			transform.position += finalMoveSpeed * Time.deltaTime;
-
-			UpdateMovingBoolean((x != 0f || y != 0f));
+			Movement();
+			RotateTowardsMouse();
 		}
 	}
 
-	private void UpdateMovingBoolean(bool moving)
+	private void Movement()
 	{
-		anim.SetBool("Moving", moving);
+		float x = Input.GetAxisRaw("Horizontal");
+		float y = Input.GetAxisRaw("Vertical");
+		Vector3 finalMoveSpeed = new Vector3(x, y, 0f).normalized * movespeed;
+
+		transform.position += finalMoveSpeed * Time.deltaTime;
 	}
 
+	private void RotateTowardsMouse()
+	{
+		//rotation
+		Vector3 mousePos = Input.mousePosition;
+		mousePos.z = 0;
+
+		Vector3 objectPos = Camera.main.WorldToScreenPoint(transform.position);
+		mousePos.x = mousePos.x - objectPos.x;
+		mousePos.y = mousePos.y - objectPos.y;
+
+		float angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
+		transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle - 90f));
+	}
 }

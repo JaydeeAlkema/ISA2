ad2bf3aaf20485c8a9bfcda94c4e9c945c6c27db
Jaydee Alkema <dev.jaydeealkema@gmail.com>
2020-06-26 16:59:57 +0200

    Remove shooter functionality and revert to basic behaviour

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -3,25 +3,14 @@ using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class PlayerController : MonoBehaviourPun, IDamageable
+public class PlayerController : MonoBehaviourPun
 {
 	[SerializeField] private Transform bodySprite = default;        // Reference to the body sprite. This will be the part that rotates.
-	[SerializeField] private float health = 100f;                   // How much health the player has.
 	[SerializeField] private float movespeed = 1f;                  // Movement speed...
-	[Space]
-	[SerializeField] private Transform firePoint = default;         // Transform point from which we fire .
-	[SerializeField] private string shootInputString = "Fire1";     // Which input to check for to fire your weapon.
-	[SerializeField] private LayerMask hitLayer = default;          // Which layers 
-	[Space]
-	[SerializeField] private Vector3 respawnPos = default;          // Respawn position.
-
-	SpriteRenderer spriteRenderer = default;
 
 	private void OnEnable()
 	{
 		gameObject.name = PhotonNetwork.LocalPlayer.NickName;
-		respawnPos = transform.position;
-		spriteRenderer = bodySprite.GetComponent<SpriteRenderer>();
 	}
 
 	private void Update()
@@ -30,9 +19,6 @@ public class PlayerController : MonoBehaviourPun, IDamageable
 		{
 			Movement();
 			RotateTowardsMouse();
-
-			// Listen for shoot input to be able to shoot.
-			if(Input.GetButtonDown(shootInputString)) Shoot();
 		}
 	}
 
@@ -63,66 +49,5 @@ public class PlayerController : MonoBehaviourPun, IDamageable
 
 		float angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
 		bodySprite.rotation = Quaternion.Euler(new Vector3(0, 0, angle - 90f));
-	}
-
-	/// <summary>
-	/// Fires a ray towards the direction the player is facing.
-	/// Then does some stuff when hitting stuff...
-	/// </summary>
-	private void Shoot()
-	{
-		RaycastHit2D hit;
-		hit = Physics2D.Raycast(firePoint.position, bodySprite.transform.up, 1000f, hitLayer);
-
-		if(hit)
-		{
-			Debug.Log("We hit: " + hit.collider.name);
-			Debug.DrawLine(firePoint.position, hit.point, Color.green, 0.1f);
-
-			if(hit.collider.name != gameObject.name)
-			{
-				hit.collider.GetComponent<IDamageable>()?.Damage(25f);
-			}
-		}
-		else
-		{
-			Debug.Log("We hit Nothing!");
-			Debug.DrawLine(firePoint.position, bodySprite.transform.up * 1000f, Color.red, 0.1f);
-		}
-	}
-
-	/// <summary>
-	/// IDamageable interface Damage method implementation.
-	/// </summary>
-	/// <param name="value"></param>
-	public void Damage(float value)
-	{
-		StartCoroutine(OnHitAnimation());
-		health -= value;
-
-		if(health <= 0)
-		{
-			Respawn();
-		}
-	}
-
-	/// <summary>
-	/// Respawns the player to its respawn point.
-	/// </summary>
-	private void Respawn()
-	{
-		transform.localPosition = Vector3.zero + respawnPos;
-		health = 100;
-	}
-
-	/// <summary>
-	/// A very simple on hit "animation".
-	/// </summary>
-	/// <returns></returns>
-	private IEnumerator OnHitAnimation()
-	{
-		spriteRenderer.color = Color.red;
-		yield return new WaitForSeconds(0.15f);
-		spriteRenderer.color = Color.white;
 	}
 }
